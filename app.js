34b9af57382c0fcb1931101265e8ca55056bb6a1
James <43918220+jamxu88@users.noreply.github.com>
2019-02-28 18:55:42 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ client.on("ready", () => {
   console.log("Meme Team Operator");
 });
 client.on("message",(message) => {
- if(message.content.includes === "<@408064561611276290>") {
+ if(message.content.includes("<@408064561611276290>")) {
    message.channel.send("https://cdn.discordapp.com/attachments/516393595285143553/550822391903944764/Screenshot_1057.png")
-  }
+ }else
+ if(message.content.startsWith("hi") || message.content.startsWith("hello") || message.content.startsWith("Hi") || message.content.startsWith("Hello")) {
+   message.channel.send("hi")
+ }
 });
 client.login(process.env.BOT_TOKEN);
