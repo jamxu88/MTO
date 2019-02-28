789c3919a621a900e47046b24fcc10db2ee68038
James <43918220+jamxu88@users.noreply.github.com>
2019-02-28 18:58:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ client.on("message",(message) => {
  if(message.content.includes("<@408064561611276290>")) {
    message.channel.send("https://cdn.discordapp.com/attachments/516393595285143553/550822391903944764/Screenshot_1057.png")
  }else
- if(message.content.startsWith("hi") || message.content.startsWith("hello") || message.content.startsWith("Hi") || message.content.startsWith("Hello")) {
+ if(message.content.startsWith("hi") || message.content.startsWith("hello") || message.content.startsWith("Hi") || message.content.startsWith("Hello") && message.author.id !== 408064561611276290) {
    message.channel.send("hi")
  }
 });
